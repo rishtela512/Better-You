aea006c2dd15373af033491b4c28a0272d3fe008
johnq0 <161163878+johnq0@users.noreply.github.com>
2024-11-22 21:20:36 -0600

    title+login
    title login/signup

---
+++ betteryou/app/src/main/java/edu/utsa/cs3443/betteryou/HomeActivity.java
@@ -0,0 +1,15 @@
+package edu.utsa.cs3443.betteryou;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.utsa.cs3443.betteryou.R;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
